7e359473481b0590709a3811ad649c3623922724
Kasper Sacharias Eenberg <kse@clearhaus.com>
2018-04-13 14:09:15 +0200

    Add validator specs, update validator

--- lib/pedicel/validator.rb
+++ lib/pedicel/validator.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 require 'dry-validation'
 require 'base64'
+require 'openssl'
 
 module Pedicel
+  # Validation class for ApplePay Payment Token and associated data:
+  # https://developer.apple.com/library/content/documentation/PassKit/Reference/PaymentTokenJSON/PaymentTokenJSON.html
+  # This purposefully only does syntactic validation (as opposed to semantic).
   class Validator
     class Error < StandardError; end
     class TokenFormatError < Error; end
@@ -12,25 +18,116 @@ module Pedicel
       hex?: 'invalid hex',
       pan?: 'invalid pan',
       yymmdd?: 'invalid date format YYMMDD',
-    }
+      ecPublicKey?: 'is not a EC public key',
+      PKCS7Signature?: 'is not a PKCS7 Signature',
+      ECI?: 'not an ECI indicator',
+      hexsha256?: 'not hex-encoded SHA256',
+      base64sha256?: 'not base64-encoded SHA256'
+    }.freeze
 
+    # Custom predicates for validation.
+    # Some predicates statements have been unfolded for unit-testing purposes,
+    # since _simplecov_ cannot check which branches have been used.
     module Predicates
       include Dry::Logic::Predicates
 
-      predicate(:base64?) { |value| !Base64.decode64(value).nil? rescue false }
+      predicate(:base64?) do |value|
+        begin
+          !Base64.strict_decode64(value).nil?
+        rescue StandardError => _
+          false
+        end
+      end
 
-      predicate(:hex?) { |value| !Regexp.new(/\A[a-f0-9-]*\z/i).match(value).nil? }
+      predicate(:hex?) do |value|
+        if Regexp.new(/\A[a-f0-9-]*\z/i).match(value).nil?
+          false
+        else
+          true
+        end
+      end
 
-      predicate(:pan?) { |value| !Regexp.new(/\A[0-9]{13,19}\z/).match(value).nil? }
+      # Reference: https://en.wikipedia.org/wiki/Payment_card_number
+      predicate(:pan?) do |value|
+        if Regexp.new(/\A[0-9]{12,19}\z/).match(value).nil?
+          false
+        else
+          true
+        end
+      end
 
       predicate(:yymmdd?) do |value|
-        return false unless value.length == 6
-        Time.new(value[0..1],value[2..3], value[4..5]).is_a?(Time) rescue false
+        unless value.length == 6 # rubocop:disable Style/IfUnlessModifier
+          return false
+        end
+
+        begin
+          Time.new(value[0..1], value[2..3], value[4..5]).is_a?(Time)
+        rescue StandardError => _
+          false
+        end
+      end
+
+      predicate(:ecPublicKey?) do |value|
+        decoded = Base64.decode64(value)
+        begin
+          OpenSSL::PKey::EC.new(decoded).check_key
+        rescue StandardError => _
+          false
+        end
+      end
+
+      predicate(:PKCS7Signature?) do |value|
+        decoded = Base64.decode64(value)
+        begin
+          OpenSSL::PKCS7.new(decoded)
+          true
+        rescue StandardError => _
+          false
+        end
+      end
+
+      # Check if `value` is 2 digits.
+      predicate(:ECI?) do |value|
+        if !Regexp.new('\A\d{2}\z').match(value).nil?
+          true
+        else
+          false
+        end
+      end
+
+      # Check that length is 256bits, that is 64 hex digits.
+      # Depends on :hex? check.
+      predicate(:hexsha256?) do |value|
+        if value.length == 64 # rubocop:disable Style/RedundantConditional
+          true
+        else
+          false
+        end
+      end
+
+      predicate(:base64sha256?) do |value|
+        if value.length != 44
+          false
+        elsif !value.end_with?('=')
+          false
+        else
+          true
+        end
       end
     end
 
     TokenSchema = Dry::Validation.Schema do
       configure do
+        # NOTE: This option removes/sanitizes hash element not mentioned/tested.
+        # Hurray for good documentation.
+        config.input_processor = :json
+
+        # In theory, I would guess that :strict below would cause a failure if
+        # untested keys were encountered, however this appears to not be the
+        # case. Anyways, it's (of course) not documented.
+        # config.hash_type = :strict
+
         predicates(Predicates)
         def self.messages
           super.merge(en: { errors: DRY_CUSTOM_PREDICATE_ERRORS })
@@ -40,27 +137,30 @@ module Pedicel
       required(:data).filled(:str?, :base64?)
 
       required(:header).schema do
-        optional(:applicationData).filled(:str?, :hex?)
+        optional(:applicationData).filled(:str?, :hex?, :hexsha256?)
 
-        optional(:ephemeralPublicKey).filled(:str?, :base64?)
+        optional(:ephemeralPublicKey).filled(:str?, :base64?, :ecPublicKey?)
         optional(:wrappedKey).filled(:str?, :base64?)
-        rule('ephemeralPublicKey xor wrappedKey': [:ephemeralPublicKey, :wrappedKey]) do |e, w|
+
+        rule('ephemeralPublicKey xor wrappedKey':
+             %i[ephemeralPublicKey wrappedKey]) do |e, w|
           e.filled? ^ w.filled?
         end
 
-        required(:publicKeyHash).filled(:str?, :base64?)
+        required(:publicKeyHash).filled(:str?, :base64?, :base64sha256?)
 
         required(:transactionId).filled(:str?, :hex?)
       end
 
-      required(:signature).filled(:str?, :base64?)
+      required(:signature).filled(:str?, :base64?, :PKCS7Signature?)
 
-      required(:version).filled(:str?, included_in?: ['EC_v1', 'RSA_v1'])
+      required(:version).filled(:str?, included_in?: %w[EC_v1 RSA_v1])
     end
 
-    # Pedicel::Validator::TokenSchema.call({data: 'asdf', header: {ephemeralPublicKey: 'e', publicKeyHash: 'p', transactionId: 'f'}, signature: 's', version: 'EC_v1'})
-
+    # rubocop:disable Metrics/BlockLength
     TokenDataSchema = Dry::Validation.Schema do
+      # rubocop:enable Metrics/BlockLength
+
       configure do
         predicates(Predicates)
         def self.messages
@@ -80,59 +180,61 @@ module Pedicel
 
       required(:deviceManufacturerIdentifier).filled(:str?, :hex?)
 
-      required(:paymentDataType).filled(:str?, included_in?: ['3DSecure', 'EMV'])
+      required(:paymentDataType).filled(:str?,
+                                        included_in?: %w[3DSecure EMV])
 
       required(:paymentData).schema do
         optional(:onlinePaymentCryptogram).filled(:str?, :base64?)
-        optional(:eciIndicator).filled(:str?)
+        optional(:eciIndicator).filled(:str?, :ECI?)
 
         optional(:emvData).filled(:str?, :base64?)
         optional(:encryptedPINData).filled(:str?, :hex?)
       end
 
-      rule('consistent paymentDataType and paymentData': [:paymentDataType, [:paymentData, :onlinePaymentCryptogram]]) do |t, cryptogram|
+      rule(
+        'paymentDataType affects paymentData':
+         [:paymentDataType, %i[paymentData onlinePaymentCryptogram]]
+      ) do |t, cryptogram|
+
         t.eql?('3DSecure') > cryptogram.filled?
       end
     end
 
-    # Pedicel::Validator::TokenDataSchema.call(
-    #   applicationPrimaryAccountNumber: '1234567890123',
-    #   applicationExpirationDate: '101112',
-    #   currencyCode: '123',
-    #   transactionAmount: 12.34,
-    #   cardholderName: 'asdf',
-    #   deviceManufacturerIdentifier: 'adsf',
-    #   paymentDataType: 'asdf',
-    # )
-
-    def self.validate_token(token, now: Time.now)
+    def self.validate_token(token)
       validation = TokenSchema.call(token)
 
-      raise TokenFormatError, validation.hints.map{|key,msg| "#{key} #{msg}"}.join(', and ') unless validation.errors.empty?
+      unless validation.success?
+        raise TokenFormatError, format_errors(validation)
+      end
 
       true
     end
 
-    def self.valid_token?(token, now: Time.now)
-      validate_token(token, now: now) rescue false
+    def self.valid_token?(token)
+      validate_token(token)
+    rescue StandardError => _
+      false
     end
 
     def self.validate_token_data(token_data)
       validation = TokenDataSchema.call(token_data)
 
-      raise TokenDataFormatError, validation.hints.map{|key,msg| "#{key} #{msg}"}.join(', and ') unless validation.errors.empty?
+      unless validation.success?
+        raise TokenDataFormatError, format_errors(validation)
+      end
 
       true
     end
 
     def self.valid_token_data?(token_data)
-      validate_token_data(token_data) rescue false
+      validate_token_data(token_data)
+    rescue StandardError => _
+      false
     end
 
-    def validate_content(now: Time.now)
-      raise ReplayAttackError, "token signature time indicates a replay attack (age #{now-cms_signing_time})" unless signing_time_ok?(now: now)
-
-      raise SignatureError unless valid_signature?
+    def self.format_errors(validation)
+      validation.errors.map { |key, msg| "#{key}: #{msg}\n" }
+                .join(', and ')
     end
   end
 end
---
+++ spec/validator_spec.rb
@@ -0,0 +1,264 @@
+# frozen_string_literal: true
+
+require 'pedicel'
+require 'pedicel/validator'
+require 'pedicel-pay'
+
+require 'json'
+require 'digest'
+
+require 'pry'
+
+module Pedicel
+  class ValidationFactory
+    def self.valid_token
+      _, _, token = PedicelPay::Helper.generate_all
+      token
+    end
+
+    def self.token_with_pan(pan)
+      _, _, token = PedicelPay::Helper.generate_all(
+        token_data: {
+          pan: pan
+        }
+      )
+      token
+    end
+  end
+end
+
+# rubocop:disable Metrics/BlockLength
+describe 'Pedicel::Validator.validate_token_data' do
+  context 'with valid data' do
+    it 'should validate' do
+      data = Pedicel::ValidationFactory.valid_token.unencrypted_data
+
+      expect(Pedicel::Validator.validate_token_data(data.to_hash)).to be true
+      expect(Pedicel::Validator.valid_token_data?(data.to_hash)).to be true
+    end
+  end
+
+  it 'should support shorthand notation' do
+    expect(Pedicel::Validator.valid_token_data?({})).to be false
+  end
+
+  context 'with invalid pan' do
+    it 'should reject if shorter than 12' do
+      token = Pedicel::ValidationFactory.token_with_pan('24153681224')
+                                        .unencrypted_data
+
+      expect do
+        Pedicel::Validator.validate_token_data(token.to_hash)
+      end.to raise_error(Pedicel::Validator::TokenDataFormatError)
+        .with_message(/applicationPrimaryAccountNumber.+invalid pan/i)
+    end
+
+    it 'should reject if longer than 19' do
+      token = Pedicel::ValidationFactory.token_with_pan('24153681224518264891')
+                                        .unencrypted_data
+
+      expect do
+        Pedicel::Validator.validate_token_data(token.to_hash)
+      end.to raise_error(Pedicel::Validator::TokenDataFormatError)
+        .with_message(/applicationPrimaryAccountNumber.+invalid pan/i)
+    end
+
+    it 'should reject if contains non-digits' do
+      token = Pedicel::ValidationFactory.token_with_pan('122492765917264a')
+                                        .unencrypted_data
+
+      expect do
+        Pedicel::Validator.validate_token_data(token.to_hash)
+      end.to raise_error(Pedicel::Validator::TokenDataFormatError)
+        .with_message(/applicationPrimaryAccountNumber.+invalid pan/i)
+    end
+  end
+
+  it 'should reject if eciIndicator is not two digits' do
+    %w[x1 1 231].each do |value|
+      data = Pedicel::ValidationFactory.valid_token.unencrypted_data.to_hash
+      data[:paymentData][:eciIndicator] = value
+
+      expect do
+        Pedicel::Validator.validate_token_data(data.to_hash)
+      end.to raise_error(Pedicel::Validator::TokenDataFormatError)
+        .with_message(/eciIndicator.+not an eci indicator/i)
+    end
+  end
+
+  it 'should reject invalid time' do
+    %w[11111 1111111 999999].each do |value|
+      data = Pedicel::ValidationFactory.valid_token.unencrypted_data.to_hash
+      data[:applicationExpirationDate] = value
+
+      expect do
+        Pedicel::Validator.validate_token_data(data.to_hash)
+      end.to raise_error(Pedicel::Validator::TokenDataFormatError)
+        .with_message(/applicationExpirationDate.+invalid date format/i)
+    end
+  end
+end
+
+describe 'Pedicel::Validator.validate_token' do
+  context 'with valid token' do
+    it 'should validate token' do
+      token = Pedicel::ValidationFactory.valid_token
+
+      token_hash = JSON.parse(token.to_json, symbolize_names: true)
+      expect(Pedicel::Validator.validate_token(token.to_hash)).to be true
+
+      expect(Pedicel::Validator.valid_token?(token_hash)).to be true
+    end
+  end
+
+  it 'should support shorthand notation' do
+    expect(Pedicel::Validator.valid_token?({})).to be false
+  end
+
+  context 'should check for base64, so' do
+    it 'should fail on non base64-encoded text' do
+      token = Pedicel::ValidationFactory.token_with_pan('24153681224').to_hash
+
+      token['data'] = token['data'][0..-2]
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/data.*invalid base64/i)
+    end
+
+    it 'should fail on non base64-encoded signature' do
+      token = Pedicel::ValidationFactory.token_with_pan('24153681224').to_hash
+
+      token['signature'] = token['signature'][0..-2]
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/signature.*invalid base64/i)
+    end
+
+    it 'should fail on non base64-encoded ephemeralPublicKey' do
+      token = Pedicel::ValidationFactory.token_with_pan('24153681224').to_hash
+
+      token['header']['ephemeralPublicKey'] =
+        token['header']['ephemeralPublicKey'][0..-2]
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/ephemeralpublickey.+invalid base64/i)
+    end
+
+    it 'should fail on non base64-encoded publicKeyHash' do
+      token = Pedicel::ValidationFactory.token_with_pan('24153681224').to_hash
+
+      token['header']['publicKeyHash'] =
+        token['header']['publicKeyHash'][0..-2]
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/publicKeyHash.+invalid base64/i)
+    end
+  end
+
+  context 'should check for hex encoding:' do
+    it 'should fail for non-hex applicationData' do
+      token = Pedicel::ValidationFactory.token_with_pan('24153681224').to_hash
+
+      token['header']['applicationData'] = 'this is not hex-encoded'
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/applicationData.+invalid hex/i)
+    end
+
+    it 'should fail for non-hex transactionId' do
+      token = Pedicel::ValidationFactory.valid_token.to_hash
+
+      token['header']['transactionId'] = 'this is very much not hex-encoded'
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/transactionId.+invalid hex/i)
+    end
+  end
+
+  context 'should check hashes:' do
+    it 'should fail on non-sha256 applicationData' do
+      token = Pedicel::ValidationFactory.valid_token.to_hash
+
+      digest = Digest::SHA512.hexdigest 'Testing string'
+
+      token['header']['applicationData'] = digest
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/applicationData.+not hex-encoded SHA256/i)
+    end
+
+    it 'should succeed on sha256 applicationData' do
+      token = Pedicel::ValidationFactory.valid_token.to_hash
+
+      digest = Digest::SHA256.hexdigest 'Testing string'
+
+      token['header']['applicationData'] = digest
+
+      expect(Pedicel::Validator.valid_token?(token)).to be true
+    end
+
+    it 'should fail on non-sha256 publicKeyHash' do
+      token = Pedicel::ValidationFactory.valid_token.to_hash
+
+      digest = Digest::SHA512.base64digest 'Testing string'
+
+      token['header']['publicKeyHash'] = digest
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/publicKeyHash.+not base64-encoded SHA256/i)
+    end
+
+    it 'should fail on base64-encoded publicKeyHash that is too long' do
+      token = Pedicel::ValidationFactory.valid_token.to_hash
+
+      token['header']['publicKeyHash'] =
+        Base64.strict_encode64(Random.new.bytes(33))
+
+      expect do
+        Pedicel::Validator.validate_token(token)
+      end.to raise_error(Pedicel::Validator::TokenFormatError)
+        .with_message(/publicKeyHash.+not base64-encoded sha256/i)
+    end
+  end
+
+  it 'should fail if signature is not PKCS7' do
+    token = Pedicel::ValidationFactory.valid_token.to_hash
+    fakesig = Base64.strict_encode64('This is not a signature')
+    token['signature'] = fakesig
+
+    expect do
+      Pedicel::Validator.validate_token(token)
+    end.to raise_error(Pedicel::Validator::TokenFormatError)
+      .with_message(/signature.+PKCS7/i)
+  end
+
+  it 'should fail if ephemeralPublicKey is not X.509 certificate' do
+    token = Pedicel::ValidationFactory.valid_token.to_hash
+    key = OpenSSL::PKey::RSA.new 4096
+
+    token['header']['ephemeralPublicKey'] =
+      Base64.strict_encode64(key.public_key.to_der)
+
+    expect do
+      Pedicel::Validator.validate_token(token)
+    end.to raise_error(Pedicel::Validator::TokenFormatError)
+      .with_message(/ephemeralPublicKey.+not a EC public key/i)
+  end
+end
+# rubocop:enable Metrics/BlockLength
