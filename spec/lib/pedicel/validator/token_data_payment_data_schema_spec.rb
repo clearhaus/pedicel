77cb083687865527716f17b03f9e35dada351d4a
Kasper Sacharias Eenberg <kse@clearhaus.com>
2018-06-27 12:20:37 +0200

    Use symbols in validator.rb

--- spec/lib/pedicel/validator/token_data_payment_data_schema_spec.rb
+++ spec/lib/pedicel/validator/token_data_payment_data_schema_spec.rb
@@ -4,15 +4,15 @@ require 'lib/pedicel/validator/helper'
 
 describe 'Pedicel::Validator::TokenDataPaymentDataSchema' do
   let(:tdpds) { Pedicel::Validator::TokenDataPaymentDataSchema }
-  let(:token_data_payment_data_h) { JSON.parse(token.unencrypted_data.to_hash.to_json)['paymentData'] }
+  let(:token_data_payment_data_h) { JSON.parse(token.unencrypted_data.to_hash.to_json, symbolize_names: true)[:paymentData] }
   subject { token_data_payment_data_h }
 
-  %w(
-      onlinePaymentCryptogram
-      eciIndicator
-      emvData
-      encryptedPINData
-  ).each do |payment_data_string_key|
+  %i[
+    onlinePaymentCryptogram
+    eciIndicator
+    emvData
+    encryptedPINData
+  ].each do |payment_data_string_key|
     it "errs when #{payment_data_string_key} is not a string" do
       token_data_payment_data_h[payment_data_string_key] = 42
 
@@ -22,20 +22,20 @@ describe 'Pedicel::Validator::TokenDataPaymentDataSchema' do
 
   context 'onlinePaymentCryptogram' do
     it 'may be present' do
-      token_data_payment_data_h['onlinePaymentCryptogram'] = 'validBase64='
+      token_data_payment_data_h[:onlinePaymentCryptogram] = 'validBase64='
       is_expected.to satisfy_schema(tdpds)
     end
 
     it 'may be absent' do
-      token_data_payment_data_h.delete('onlinePaymentCryptogram')
+      token_data_payment_data_h.delete(:onlinePaymentCryptogram)
       is_expected.to satisfy_schema(tdpds)
     end
 
     it 'errs when not Base64' do
-      %w(% fooo= f===).each do |invalid_value|
-        token_data_payment_data_h['onlinePaymentCryptogram'] = invalid_value
+      %w[% fooo= f===].each do |invalid_value|
+        token_data_payment_data_h[:onlinePaymentCryptogram] = invalid_value
 
-        is_expected.to dissatisfy_schema(tdpds, 'onlinePaymentCryptogram' => ['must be Base64'])
+        is_expected.to dissatisfy_schema(tdpds, onlinePaymentCryptogram: ['must be Base64'])
       end
     end
   end
@@ -52,10 +52,10 @@ describe 'Pedicel::Validator::TokenDataPaymentDataSchema' do
     end
 
     it 'errs when invalid' do
-      %w(1 123 1A).each do |invalid_value|
-        token_data_payment_data_h['eciIndicator'] = invalid_value
+      %w[1 123 1A].each do |invalid_value|
+        token_data_payment_data_h[:eciIndicator] = invalid_value
 
-        is_expected.to dissatisfy_schema(tdpds, 'eciIndicator' => ['must be an ECI'])
+        is_expected.to dissatisfy_schema(tdpds, eciIndicator: ['must be an ECI'])
       end
     end
   end
@@ -72,29 +72,29 @@ describe 'Pedicel::Validator::TokenDataPaymentDataSchema' do
     end
 
     it 'errs when not Base64' do
-      %w(% fooo= f===).each do |invalid_value|
-        token_data_payment_data_h['emvData'] = invalid_value
+      %w[% fooo= f===].each do |invalid_value|
+        token_data_payment_data_h[:emvData] = invalid_value
 
-        is_expected.to dissatisfy_schema(tdpds, 'emvData' => ['must be Base64'])
+        is_expected.to dissatisfy_schema(tdpds, emvData: ['must be Base64'])
       end
     end
   end
 
   context 'encryptedPINData' do
     it 'may be present' do
-      token_data_payment_data_h['encryptedPINData'] = 'a1b2c3d4e5f6'
+      token_data_payment_data_h[:encryptedPINData] = 'a1b2c3d4e5f6'
       is_expected.to satisfy_schema(tdpds)
     end
 
     it 'may be absent' do
-      token_data_payment_data_h.delete('encryptedPINData')
+      token_data_payment_data_h.delete(:encryptedPINData)
       is_expected.to satisfy_schema(tdpds)
     end
 
     it 'errs when not hex' do
-      %w(42-42 42Z).each do |invalid_value|
-        token_data_payment_data_h['encryptedPINData'] = invalid_value
-        is_expected.to dissatisfy_schema(tdpds, 'encryptedPINData' => ['must be hex'])
+      %w[42-42 42Z].each do |invalid_value|
+        token_data_payment_data_h[:encryptedPINData] = invalid_value
+        is_expected.to dissatisfy_schema(tdpds, encryptedPINData: ['must be hex'])
       end
     end
   end
